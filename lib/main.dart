909e436f2bc163916036e805c37268079f44d8a5
NdirituG <denisndiritug@gmail.com>
2022-11-02 15:19:13 +0300

    firbase_fail

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ import 'consts/theme_data.dart';
 import 'controllers/MenuController.dart';
 import 'inner_screens/add_prod.dart';
 import 'providers/dark_theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -32,32 +35,62 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => MenuController(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) {
-            return themeChangeProvider;
-          },
-        ),
-      ],
-      child: Consumer<DarkThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: 'Grocery',
-              theme: Styles.themeData(themeProvider.getDarkTheme, context),
-              home: const MainScreen(),
-              routes: {
-                UploadProductForm.routeName: (context) =>
-                    const UploadProductForm(),
-              });
-        },
-      ),
-    );
+              home: Scaffold(
+                body: Center(
+                  child: Center(
+                    child: Text('App is being initialized'),
+                  ),
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: Center(
+                    child: Text('An error has been occured ${snapshot.error}'),
+                  ),
+                ),
+              ),
+            );
+          }
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => MenuController(),
+              ),
+              ChangeNotifierProvider(
+                create: (_) {
+                  return themeChangeProvider;
+                },
+              ),
+            ],
+            child: Consumer<DarkThemeProvider>(
+              builder: (context, themeProvider, child) {
+                return MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: 'Grocery',
+                    theme:
+                        Styles.themeData(themeProvider.getDarkTheme, context),
+                    home: const MainScreen(),
+                    routes: {
+                      UploadProductForm.routeName: (context) =>
+                          const UploadProductForm(),
+                    });
+              },
+            ),
+          );
+        });
   }
 }
